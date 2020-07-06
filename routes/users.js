fff59b7f7552f18f2f6c4090849ffbb717f8ae6d
sid <you@example.com>
2020-07-06 15:27:21 +0530

    create user schema and render pages for sign in and sign up

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,9 @@ const usersController = require('../controllers/users_controller');
 const postsController = require('../controllers/posts_controller');
 
 router.get('/profile', usersController.profile);
-router.get('/posts',postsController.posts);
+
+
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
 
 module.exports = router;
