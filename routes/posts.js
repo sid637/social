739032ce753f13f9cca205045c46dbc4002a25bd
sid <you@example.com>
2020-07-10 15:57:38 +0530

    add delete a post

--- routes/posts.js
+++ routes/posts.js
@@ -8,6 +8,6 @@ const postController = require('../controllers/posts_controller');
 // putted a check if someone is not signed will not be able to post
 router.post('/create',passport.checkAuthentication , postController.create);
 
-
+router.get('/destroy/:id', passport.checkAuthentication, postController.destroy);
 
 module.exports = router;
