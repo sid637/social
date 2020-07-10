7313d0e7ce0f97c7604d6d5c5fa57a3171f6eee0
sid <you@example.com>
2020-07-10 10:36:10 +0530

    putted a check if someone is not signed will not be able to post in router.post

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+
 const postController = require('../controllers/posts_controller');
 
-router.post('/create', postController.create);
+// putted a check if someone is not signed will not be able to post
+router.post('/create',passport.checkAuthentication , postController.create);
 
 
 
