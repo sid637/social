8515643fe51a29cdec042177e1f3153e658e0487
sid <you@example.com>
2020-07-11 13:23:34 +0530

    setup async await and error handling

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -2,49 +2,63 @@ const Comment = require('../models/comment');
 const Post = require('../models/post');
 
 
-module.exports.create = function(req, res){
-    // req.body.post here is re from hidden input
-    Post.findById(req.body.post, function(err, post){
+module.exports.create = async function(req, res){
+   try{
+        // req.body.post here is re from hidden input
+
+        let post = await Post.findById(req.body.post);
 
         // if post exists then create a comment
-        if(post){
-            Comment.create({
-                content: req.body.content,
-                post: req.body.post,
-                user: req.user._id
-            }, function(err, comment){
+     if(post){
+        Comment.create({
+            content: req.body.content,
+            post: req.body.post,
+            user: req.user._id
+        });
 
-                if(err){
-                    console.log('Error in creating the comment');
-                    return;
-                }
+            // update
+        post.comments.push(comment);
+            // to save in the database
+        post.save();
 
-                // update
-                post.comments.push(comment);
-                // to save in the database
-                post.save();
-
-                res.redirect('/');
-
-            }); 
+        res.redirect('/');
         }
-    });
+
+       
+    }catch(err){
+       console.log('Error', err);
+       return;
+   }
+   
+    
 }
 
+
 module.exports.destroy = function(req, res){
-    Comment.findById(req.params.id, function(err, comment){
+    try{
+
+        let comment = await Comment.findById(req.params.id);
+
         // to check in the databse if comment actually exists or not
         if(comment.user == req.user.id){
+
             // we have to save postis before comment removal
+
             let postId = comment.post;
 
             comment.remove();
-                // $pull is mongo syntax, this pulls and throws the id matching with the comment id
-            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post){
+            // $pull is mongo syntax, this pulls and throws the id matching with the comment id
+            let post =  Post.findByIdAndUpdate(postId,
+                 { $pull: {comments: req.params.id}});
+
                 return res.redirect('back');
-            });
+            
         }else{
-            return res.redirect('back');
+                return res.redirect('back');
         }
-    });
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,38 +1,66 @@
 const Post = require("../models/post");
 const User = require('../models/user');
-module.exports.home = function(req,res){
-    // console.log(req.cookies);
-    // res.cookie('user_id',25);
+// module.exports.home =  function(req,res){
+//     // console.log(req.cookies);
+//     // res.cookie('user_id',25);
 
-    // Post.find({}, function(err, posts){
+//     // Post.find({}, function(err, posts){
         
-    //     return res.render('home.ejs',{
-    //         title: "Codeial | Home",
-    //         posts: posts
-    //     });
+//     //     return res.render('home.ejs',{
+//     //         title: "Codeial | Home",
+//     //         posts: posts
+//     //     });
 
-    // finding the post and populating the user of each post
-    // just need to prepopulate the user and we need to do call back in exec
+//     // finding the post and populating the user of each post
+//     // just need to prepopulate the user and we need to do call back in exec
     
-    //nested population 
-    Post.find({}).populate('user').populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    }).exec(function(err, posts){
+//     //nested population 
+//     Post.find({}).populate('user').populate({
+//         path: 'comments',
+//         populate: {
+//             path: 'user'
+//         }
+//     }).exec(function(err, posts){
 
-        User.find({}, function(err, users){
-            return res.render('home.ejs',{
-                title: "Codeial | Home",
-                posts: posts,
-                all_users: users
+//         User.find({}, function(err, users){
+//             return res.render('home.ejs',{
+//                 title: "Codeial | Home",
+//                 posts: posts,
+//                 all_users: users
+//             });
+
+//         });
+
+        
+
+//     });
+    
+// }
+//** */ with async await
+module.exports.home = async function(req, res){
+
+    try{
+         // populate the user of each post
+    let posts = await Post.find({})
+    .populate('user').populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
             });
 
-        });
+    let users = await User.find({});
 
-        
-
+    return res.render('home.ejs',{
+        title: "Codeial | Home",
+        posts: posts,
+        all_users: users
     });
-    
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+
+       
 }
